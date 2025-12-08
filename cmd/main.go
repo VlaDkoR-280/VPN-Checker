0e36f72fee7f5107d0a6f9f6b1e366a86c348382
korolevvd <korolevvd@tutu.ru>
2025-12-09 00:53:50 +0300

    - created adapter - created base main func to test adapter

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"github.com/VlaDkoR-280/VPN-Checker/internal/adapters/vpn"
+	"log"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	adapter, errInit := vpn.InitAdapter(ctx, "test")
+	if errInit != nil {
+		log.Fatal(errInit)
+	}
+
+	log.Println("VPN adapter initialized")
+
+	ip, errGetIP := adapter.GetIPAddress(ctx)
+	if errGetIP != nil {
+		log.Fatal(errGetIP)
+	}
+
+	log.Println("ip address: ", ip)
+}
---
+++ internal/adapters/vpn/adapter.go
@@ -0,0 +1,135 @@
+package vpn
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"os/exec"
+	"strings"
+)
+
+type Adapter struct {
+	ns string
+}
+
+func InitAdapter(ctx context.Context, prefixName string) (*Adapter, error) {
+	a := &Adapter{
+		ns: fmt.Sprintf("%s-vpn-checker", prefixName),
+	}
+
+	_, err := exec.CommandContext(ctx, "ip", "netns", "add", a.ns).Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link add veth1 type veth peer name veth2").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link add veth3 type veth peer name veth4").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth2 netns", a.ns).Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth4 netns", a.ns).Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link add name br-vpn type bridge").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "addr add 192.168.89.1/24 dev br-vpn").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set br-vpn up").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth1 master br-vpn").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth3 master br-vpn").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth1 up").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "link set veth3 up").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "echo", "echo 1 > /proc/sys/net/ipv4/ip_forward").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "iptables", "-t nat -A POSTROUTING -o eth0 -j MASQUERADE").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "iptables", "-A FORWARD -i br-vpn -o eth0 -j ACCEPT").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "iptables", "-A FORWARD -i eth0 -o br-vpn -j ACCEPT").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "netns exec", a.ns, "ip link set veth2 up").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "netns exec", a.ns, "ip link set veth4 up").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "netns exec", a.ns, "ip addr add 192.168.89.2/24 dev veth2").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = exec.CommandContext(ctx, "ip", "netns exec", a.ns, "ip route add default via 192.168.89.1").Output()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return a, nil
+}
+
+func (a *Adapter) TestBaseConnection(ctx context.Context) error {
+	_, err := exec.CommandContext(ctx, "ip", "link set veth1 down").Output()
+	return errors.WithStack(err)
+
+}
+
+func (a *Adapter) GetIPAddress(ctx context.Context) (string, error) {
+	out, err := exec.CommandContext(ctx, "ip", "netns exec", a.ns, "").Output()
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	return strings.TrimSpace(string(out)), nil
+}
